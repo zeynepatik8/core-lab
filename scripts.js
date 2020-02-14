3082c873ea7c78c40f916ba07044755232d61118
zeynepatik8 <60242153+zeynepatik8@users.noreply.github.com>
2020-02-14 17:03:08 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// This is where the javascript will live.
+// Using Chrome developer tools, if you look at the console, you can see the message appearing on the console.
+
+console.log('Is the scripts file working?');
